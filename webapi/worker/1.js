7c51ab61895afdd1c83f35b362b735da8baec2c3
bindy <83840779@qq.com>
2022-03-25 19:14:41 +0800

    3.25

---
+++ webapi/worker/1.js
@@ -0,0 +1,21 @@
+
+
+self.addEventListener('message', function (e) {
+    var data = e.data;
+    console.log('worker:', data)
+    switch (data.cmd) {
+        case 'start':
+            setTimeout(() => {
+                self.postMessage({ cmd: 'wstart' });
+            }, 2000);
+            break;
+        case 'stop':
+            setTimeout(() => {
+                self.postMessage({ cmd: 'wstop' });
+                self.close(); // Terminates the worker.
+            }, 2000);
+            break;
+        default:
+            self.postMessage('Unknown command: ' + data);
+    };
+}, false);
